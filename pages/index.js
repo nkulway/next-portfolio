6b84c9f750a48f2b55c9ce2426eb1a75d03ba088
Nick Kulway <nkulway@gmail.com>
2022-09-12 21:39:07 -0400

    build out index frame

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import About from "../components/About";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import testImage from "../public/vercel.svg";
+import testImage from "../public/profilePicture.jpeg";
 import { useRef } from "react";
 
 export default function Home() {
@@ -19,17 +19,17 @@ export default function Home() {
   };
 
   const handleClickToExperience = () => {
-    experienceRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    experienceRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const handleClickToProjects = () => {
-    projectRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
+    projectRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const handleClickToContact = () => {
-    contactRef.current?.scrollIntoView({ behavior: "smooth" })
-  }
-  
+    contactRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <Head>
@@ -37,33 +37,39 @@ export default function Home() {
         <meta name='description' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <nav className={styles.nav}>
-        <div className={styles.lineContainer}>
-          <div className={styles.navLinks}>
-            <div className={styles.imageWrapper}>
-              <Image
-                className={styles.avatar}
-                src={testImage}
-                alt='A picture of Nick Kulway'
-              />
+      <div className={styles.navParent}>
+        <div className={styles.navContainer}>
+          <nav className={styles.nav}>
+            <div className={styles.lineContainer}>
+              <div className={styles.navLinks}>
+                <div className={styles.imageWrapper}>
+                  <Image
+                    className={styles.avatar}
+                    src={testImage}
+                    alt='A picture of Nick Kulway'
+                  />
+                </div>
+                <ul className={styles.linkContainer}>
+                  <li onClick={handleClickToAbout}>About</li>
+                  <li onClick={handleClickToExperience}>Experience</li>
+                  <li onClick={handleClickToProjects}>Projects</li>
+                  <li onClick={handleClickToContact}>Contact</li>
+                </ul>
+              </div>
+              <div className={styles.greenLine}></div>
+              <div className={styles.metalLine}></div>
             </div>
-            <ul>
-              <li onClick={handleClickToAbout}>About</li>
-              <li onClick={handleClickToExperience}>Experience</li>
-              <li onClick={handleClickToProjects}>Projects</li>
-              <li onClick={handleClickToContact}>Contact</li>
-            </ul>
-          </div>
-          <div className={styles.greenLine}></div>
-          <div className={styles.metalLine}></div>
+          </nav>
         </div>
-      </nav>
-      <Main ref={{
-        ref1: aboutRef,
-        ref2: experienceRef,
-        ref3: projectRef,
-        ref4: contactRef
-      }} />
+      </div>
+      <Main
+        ref={{
+          ref1: aboutRef,
+          ref2: experienceRef,
+          ref3: projectRef,
+          ref4: contactRef,
+        }}
+      />
     </>
   );
 }
