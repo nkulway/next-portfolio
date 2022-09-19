fa71479db00445d1a2df0c79cb93003b714b92d8
Nick Kulway <nkulway@gmail.com>
2022-09-19 08:47:41 -0700

    Import projects object into card component

--- public/projects.js
+++ public/projects.js
@@ -1,12 +1,22 @@
 const projects = [
   {
+    id: "1",
     title: "Project 1",
     heading: "This is a heading for project 1",
     description: "This is project 1",
   },
   {
+    id: "2",
     title: "Project 2",
     heading: "This is a heading for project 2",
     description: "This is project 2",
   },
+  {
+    id: "3",
+    title: "Project 3",
+    heading: "This is a heading for project 3",
+    description: "This is project 3",
+  }
 ];
+
+export default projects;
