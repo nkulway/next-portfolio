73ce2968a9b88465cfe054de4d18a035404506d4
Nick Kulway <nkulway@gmail.com>
2022-09-27 21:40:59 -0400

    correct desktop view

--- components/Main.js
+++ components/Main.js
@@ -1,13 +1,10 @@
 /* eslint-disable react/display-name */
-import mainStyles from "../styles/Main.module.css";
-import React, { forwardRef, useRef } from "react";
 import About from "./About";
-import Experience from "./Experience";
-import Projects from "./Projects";
+import React, { forwardRef, useRef } from "react";
 import Contact from "./Contact";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import profileImage from "../public/profilePicture.jpeg";
+import Projects from "./Projects";
+import mainStyles from "../styles/Main.module.css";
+import Experience from "./Experience";
 
 const Main = forwardRef((_props, ref) => {
   const aboutRef = useRef(null);
@@ -34,31 +31,6 @@ const Main = forwardRef((_props, ref) => {
   const { ref1, ref2, ref3, ref4 } = ref;
   return (
     <main className={mainStyles.main}>
-      {/* <div className={styles.navParent}>
-        <div className={styles.navContainer}>
-          <nav className={styles.nav}>
-            <div className={styles.lineContainer}>
-              <div className={styles.navLinks}>
-                <div className={styles.imageWrapper}>
-                  <Image
-                    className={styles.avatar}
-                    src={profileImage}
-                    alt='A picture of Nick Kulway'
-                  />
-                </div>
-                <ul className={styles.linkContainer}>
-                  <li onClick={handleClickToAbout}>About</li>
-                  <li onClick={handleClickToExperience}>Experience</li>
-                  <li onClick={handleClickToProjects}>Projects</li>
-                  <li onClick={handleClickToContact}>Contact</li>
-                </ul>
-              </div>
-              <div className={styles.greenLine}></div>
-              <div className={styles.metalLine}></div>
-            </div>
-          </nav>
-        </div>
-      </div> */}
       <div className={mainStyles.mainContent}>
         <About ref={ref1} id='about' />
         <Experience ref={ref2} />
