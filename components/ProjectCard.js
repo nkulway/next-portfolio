52780a75620c236aee1d8bebed0509d6a027585f
Nick Kulway <nkulway@gmail.com>
2022-09-28 21:30:07 -0400

    add styles to project cards

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import dummyImage from "../public/profilePicture.jpeg";
 import projectObj from "../public/projects.js";
+import projectStyles from "../styles/Projects.module.css";
 
 const ProjectCard = () => {
   return (
     <>
       {projectObj.map((projects) => (
-        <section key={projects.id}>
+        <section key={projects.id} className={projectStyles.cardContainer}>
           <Image alt='Project Card' src={dummyImage} width={200} height={200} />
           <div>
             <h2>{projects.title}</h2>
