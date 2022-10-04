20a55302fef78c128a53e91a039dcb2aad0fe389
Nick Kulway <nkulway@gmail.com>
2022-10-03 22:19:36 -0400

    style project container

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -8,11 +8,13 @@ const ProjectCard = () => {
     <>
       {projectObj.map((projects) => (
         <section key={projects.id} className={projectStyles.cardContainer}>
-          <Image alt='Project Card' src={dummyImage} width={200} height={200} />
+          <Image alt='Project Card' src={dummyImage} width={300} height={200} />
           <div className={projectStyles.cardContent}>
             <h2>{projects.title}</h2>
             <p>{projects.heading}</p>
-            <p>{projects.description}</p>
+            <div className={projectStyles.descriptionContainer}>
+              <p>{projects.description}</p>
+            </div>
           </div>
         </section>
       ))}
